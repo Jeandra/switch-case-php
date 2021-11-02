4788132e01548368f15e6399f1de1e99077cea84
Jeandra <76909903+Jeandra@users.noreply.github.com>
2021-11-02 13:55:01 -0300

    add código

---
+++ switchcase.php
@@ -0,0 +1,17 @@
+<?php
+
+$opcao = "3";
+
+switch ($opcao) {
+    case '1':
+        echo"Opção 1 escolhida";   
+        break;
+
+        case '2':
+            echo"Opção 2 escolhida";   
+            break;    
+    
+    default:
+        echo "Opção inválida!";
+        break;
+}
